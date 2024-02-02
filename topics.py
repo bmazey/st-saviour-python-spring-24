b9bac990e9ffa07d997313d8d5a4aca860f1599d
brandon mazey <brandon@brandons-Air.localdomain>
2024-02-02 08:31:30 -0500

    fibonacci

--- topics.py
+++ topics.py
@@ -1,9 +1,33 @@
 import re
 import json
 
-if __name__ == '__main__':
-    # https://docs.python.org/3/library/re.html
+# recursive fibonacci
+def fibonacci(n: int) -> int:
+    """computes the nth number in the fibonacci sequence"""
+    if n <= 1:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
 
+# dynamic fibonacci
+def dynamic_fibonacci(n: int) -> int:
+    """computes the nth number in the fibonacci sequence"""
+    if n <= 1:
+        return 1
+
+    # create list to store computations
+    dynamic = [1, 1]
+
+    for i in range (2, n):
+        dynamic.append(dynamic[i - 1] + dynamic[i - 2])
+
+    return dynamic[n - 1]
+
+if __name__ == '__main__':
+    # fibonacci examples
+    print('recursive fibonacci: ' + str(fibonacci(40)))
+    # print('dynamic fibonacci: ' + str(dynamic_fibonacci(40)))
+
+    # https://docs.python.org/3/library/re.html
     # create a regular expression
     # expression = re.compile('ab.*')
 
@@ -14,16 +38,18 @@ if __name__ == '__main__':
     jane_id = {
         'name': 'jane doe',
         'city': 'chicago',
-        'age': '22'
+        'age': '24'
     }
 
     # add an extra key / value pair
     jane_id['phone'] = '605-477-3018'
+    # print(jane_id['phone'])
 
     # duplicates not allowed!
 
-    print(jane_id['name'])
+    # print(jane_id['name'])
 
     # meet my friend json
     john_id = json.loads('{\"name\":\"john doe\",\"city\":\"chicago\",\"age\":\"22\"}')
-    print(json.dumps(john_id))
+    # print(json.dumps(john_id))
+    # print(john_id['city'])
